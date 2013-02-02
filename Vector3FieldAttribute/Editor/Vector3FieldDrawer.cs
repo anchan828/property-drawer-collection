9731ddfcb43b9ac4b0601a694a59be47b3b6eddc
Takahiro Sudoh <pchin.taka.1127@gmail.com>
2013-02-02 17:28:05 +0900

    Update Vector3FieldAttribute/Editor/Vector3FieldDrawer.cs

--- Vector3FieldAttribute/Editor/Vector3FieldDrawer.cs
+++ Vector3FieldAttribute/Editor/Vector3FieldDrawer.cs
@@ -6,16 +6,16 @@ public sealed class Vector3FieldDrawer : PropertyDrawer {
 		
 	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
 	{
-        EditorGUIUtility.LookLikeControls ();
-
+	        EditorGUIUtility.LookLikeControls ();
+	
 		if (property.propertyType == SerializedPropertyType.Vector3){
-            property.vector3Value = EditorGUI.Vector3Field(position, label.text, property.vector3Value);
-        }
-        else if (property.propertyType == SerializedPropertyType.Vector2){
-            property.vector2Value = EditorGUI.Vector2Field(position, label.text, property.vector2Value);
-        }
-        else
-            EditorGUI.LabelField (position, label.text, "Use Compact with Vector3 or Vector2.");
+	            property.vector3Value = EditorGUI.Vector3Field(position, label.text, property.vector3Value);
+	        }
+	        else if (property.propertyType == SerializedPropertyType.Vector2){
+	            property.vector2Value = EditorGUI.Vector2Field(position, label.text, property.vector2Value);
+	        }
+	        else
+	            EditorGUI.LabelField (position, label.text, "Use Compact with Vector3 or Vector2.");
 	}
 	
 	public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
