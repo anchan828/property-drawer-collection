175a2953938af9ede8f7931c215d5ecc941894c4
Keigo Ando <anchan828@gmail.com>
2013-07-28 22:50:39 +0900

    EnumLabel追加

---
+++ EnumLabel/EnumLabelAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
+public class EnumLabelAttribute : PropertyAttribute
+{
+    public string label;
+    public EnumLabelAttribute(string label)
+    {
+        this.label = label;
+    }
+}
---
+++ EnumLabel/EnumLabelExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnumLabelExample : MonoBehaviour
+{
+
+    public enum Example
+    {
+        [EnumLabel("テスト")]
+        HIGH,
+        [EnumLabel("その２")]
+        LOW
+    }
+
+    [EnumLabel("例")]
+    public Example test = Example.HIGH;
+}
