10012321d01c23baaaca5f52557a68c415578741
kyusyukeigo <anchan828@gmail.com>
2015-04-22 18:17:28 +0900

    changed textureURL

--- PreviewTexture/PreviewTextureExample.cs
+++ PreviewTexture/PreviewTextureExample.cs
@@ -5,7 +5,7 @@ public class PreviewTextureExample : MonoBehaviour
 {
     [PreviewTexture(60)]
     public string
-        textureURL = "https://www.hogehoge.com/image.png";
+        textureURL = "http://placekitten.com/200/150";
 
     [PreviewTexture]
     public Texture hoge;
